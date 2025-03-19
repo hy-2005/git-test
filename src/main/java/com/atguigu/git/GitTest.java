a1e6c78bc61dd8f595b78314b0658d2cf0a8b773
he_river123 <1678902612@qq.com>
2025-03-19 11:34:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author hejuyao
+ * @create 2025-03-19-上午11:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
